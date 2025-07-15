79727f54a047b24e7dda5fd71bdcc2dab5253dcc
GCPP GPC 05 <gcpp.gpc05@essalud.gob.pe>
2025-07-15 08:57:31 -0500

    cambios

--- src/app/pages/admin-planificacion/admin-planificacion.component.ts
+++ src/app/pages/admin-planificacion/admin-planificacion.component.ts
@@ -11,40 +11,11 @@ import { AdmPlanificacionTableComponent } from "../../components/adm-planificaci
     CommonModule,
     MenubarComponent,
     FooterComponent,
-    SelectorComponent,
     AdmPlanificacionTableComponent
 ],
   templateUrl: './admin-planificacion.component.html',
   styleUrl: './admin-planificacion.component.scss'
 })
 export class AdminPlanificacionComponent {
-  anoSeleccionado: string | null = null;
-  mostrarTabla = false;
-  idFormulation: number | null = null;
-  idDependencySeleccionado: string | null = null; // ¡NUEVA PROPIEDAD!
-
-  // Se llama apenas se cambia el año desde el selector
-  limpiarTabla() {
-    this.mostrarTabla = false;
-    this.anoSeleccionado = null;
-    this.idDependencySeleccionado = null; // Limpiar también la dependencia
-  }
-
-  // Se llama cuando se presiona "Mostrar" o "Crear"
-  manejarBusqueda(event: { ano: string | null; dependencia: string | null; idFormulation: number | null }) {
-    if (!event.ano) return;
-
-    this.anoSeleccionado = event.ano;
-    this.idFormulation = event.idFormulation;
-    this.idDependencySeleccionado = event.dependencia; // ¡ASIGNAR EL VALOR!
-
-    // Mostrar la tabla después de un ciclo de vida
-    setTimeout(() => {
-      this.mostrarTabla = true;
-    });
-  }
-
-  onSeleccionAccion(accion: any) {
-    console.log('Acción seleccionada:', accion);
-  }
+  
 }
