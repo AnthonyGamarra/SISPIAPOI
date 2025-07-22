fd490985281c04c0605f133d709ebdb3ab1d34f2
AnthonyGamarra <agamarraj@pucp.edu.pe>
2025-07-22 15:49:11 -0500

    cambios

--- src/app/pages/valida/valida.component.ts
+++ src/app/pages/valida/valida.component.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { FooterComponent } from '../../components/footer/footer.component';
-import {MenubarComponent} from '../../components/menubar/menubar.component';
-import { Form9Component } from '../../components/form9/form9.component';
-import { Guardadof9Component } from '../../components/guardadof9/guardadof9.component';
-import {SelectoractComponent} from '../../components/selectoract/selectoract.component';
-import { RBSComponent } from '../../components/rbs/rbs.component';
-
-=======
 import { FooterComponent } from '../../components/utilities/footer/footer.component';
 import {MenubarComponent} from '../../components/utilities/menubar/menubar.component';
 import { Form9Component } from '../../components/main-components/form9/form9.component';
 import { Guardadof9Component } from '../../components/main-components/guardadof9/guardadof9.component';
 import {SelectoractComponent} from '../../components/main-components/selectoract/selectoract.component';
 import { RBSComponent } from '../../components/main-components/rbs/rbs.component';
->>>>>>> 7ebf7bf7dc1b4127e047142a17c5efdfb8272164
 @Component({
   selector: 'app-valida',
   imports: [CommonModule, FooterComponent, MenubarComponent, SelectoractComponent,Form9Component,Guardadof9Component, RBSComponent],
